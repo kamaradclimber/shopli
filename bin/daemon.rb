15464d22c19562f8bed06d21bea607df3bce76e3
Gregoire Seux <kamaradclimber@gmail.com>
2014-05-20 13:43:57 +0200

    Display timestamp for barcode reader

--- bin/daemon.rb
+++ bin/daemon.rb
@@ -6,5 +6,5 @@ require_relative '../lib/shopli.rb'
 reader = Shopli::BarcodeReader.new '/dev/input/by-id/usb-1130_USB-TMU-V1-event-kbd'
 
 Shopli::Barcode.read(reader.events_stream).each do |barcode|
-  puts "Got one: #{barcode.to_s}"
+  puts "#{Time.now.to_i} #{barcode.to_s}"
 end
